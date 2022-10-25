ec6c60d96263634250f4205b0b2b2125cc8a1d22
Naveen Kumar <naveenkumarjn@outlook.com>
2022-10-26 01:51:49 +0530

    #OM-Added logic for MultiLevelData

--- src/main/java/com/oneentropy/mapper/service/MappingService.java
+++ src/main/java/com/oneentropy/mapper/service/MappingService.java
@@ -5,6 +5,9 @@ import com.oneentropy.mapper.exceptions.InterpretException;
 import com.oneentropy.mapper.functional.PostMapFunction;
 import com.oneentropy.mapper.functional.PreMapFunction;
 import com.oneentropy.mapper.model.AttributeMap;
+import com.oneentropy.mapper.model.Data;
+import com.oneentropy.mapper.model.MultiLevelData;
+import com.oneentropy.mapper.model.SingleLevelData;
 
 import java.util.List;
 import java.util.Map;
@@ -12,12 +15,20 @@ import java.util.Map;
 public interface MappingService {
 
 
-    JsonNode mapDataToJsonNode(List<Map<String, String>> data, List<AttributeMap> attributeMapList, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+    JsonNode mapDataToJsonNode(SingleLevelData singleLevelData, List<AttributeMap> attributeMapList, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
 
-    JsonNode mapDataToJsonNode(JsonNode template, Map<String, String> data, int iteration, List<AttributeMap> attributeMapList, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+    JsonNode mapDataToJsonNode(JsonNode template, Data data, int activationLevel, List<AttributeMap> attributeMapList, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
 
-    JsonNode mapDataToJsonNode(List<Map<String, String>> data, String mappingConf,PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+    JsonNode mapDataToJsonNode(JsonNode template, SingleLevelData singleLevelData, int activationLevel, List<AttributeMap> attributeMapList, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
 
-    JsonNode mapDataToJsonNode(Map<String, String> data, String mappingConf,PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+    JsonNode mapDataToJsonNode(JsonNode template, SingleLevelData singleLevelData, int activationLevel, String mappingConf, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+
+    JsonNode mapDataToJsonNode(SingleLevelData singleLevelData, String mappingConf,PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+
+    JsonNode mapDataToJsonNode(Data data, String mappingConf,PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+
+    JsonNode mapDataToJsonNode(MultiLevelData multiLevelData, String[] mappingConf, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
+
+    JsonNode mapDataToJsonNode(JsonNode template, MultiLevelData multiLevelData, String[] mappingConf, PreMapFunction preMapFunction, PostMapFunction postMapFunction) throws InterpretException;
 
 }
